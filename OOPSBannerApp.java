3ad5bc9b1a1bad942162e407df7ef586fcf8e31a
ItsGriffon <smrshibisenthil@gmail.com>
2026-02-17 11:11:01 +0530

    UC1: Added tracking comment for PR and fixed typo

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
-
+// UC1 Assignment: Basic Output Implementation
 public class OOPSBannerApp {
 	public static void main(String[] args) {
-		System.out.printin("OOPS");
+		System.out.println("OOPS");
 	}
 }
